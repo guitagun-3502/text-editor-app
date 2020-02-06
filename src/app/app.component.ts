8320d9607975db4549db1f527379dc113f400136
guitagun-3502 <37022012+guitagun-3502@users.noreply.github.com>
2020-02-06 05:04:12 +0300

    ReadyCommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,75 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import { User } from './user.model';
+import { NgForm } from '@angular/forms';
+import { Injectable } from '@angular/core';
+import DataFromFile from '../assets/data.json';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
+@Injectable()
 export class AppComponent {
-  title = 'table-editor-app';
+  private textareaValue: string = '';
+  private saveIndex: any;
+  private dataArray = new Array<User>();
+  private newData: User = new User();
+  private data: any = new User();
+  private dataKeys: Array<string>;
+  private dataFromFile: object = DataFromFile;
+  private parsingComplete: boolean = false;
+  private parsingError: boolean = false;
+
+  parseData(parseData): any {
+    return JSON.parse(parseData);
+  }
+  loadData(parseData) {
+    try {
+      this.dataArray = this.parseData(parseData);
+      this.dataKeys = this.getKeys(this.dataArray);
+      this.parsingComplete = true;
+      this.parsingError = false;
+    } catch (e) {
+      console.info('could not parse JSON' + e);
+      this.parsingError = true;
+      this.parsingComplete = false;
+    }
+  }
+  loadDataFromFile(loadData, textArea){
+    this.dataArray = loadData;
+    textArea.value = JSON.stringify(this.dataArray);
+    this.dataKeys = this.getKeys(this.dataArray);
+    this.parsingComplete = true;
+    this.parsingError = false;
+  }
+  stringifyJSON(textArea) {
+    textArea.value = JSON.stringify(this.dataArray);
+  }
+  getData(): object {
+    return this.dataArray;
+  }
+  getKeys(obj: object): Array<string> {
+    return Object.keys(obj[0]);
+  }
+  deleteData(index){
+    this.dataArray.splice(index,1);
+  }
+  addData(inputName: string, inputYear: string) {
+    this.dataArray.push(this.newData);
+    this.newData = {};
+  }
+  editData(index) {
+    this.saveIndex = index;
+    this.data = Object.create(this.dataArray[this.saveIndex]);
+    console.log(this.data);
+  }
+  saveChanges() {
+    console.log(this.data);
+    Object.assign(this.dataArray[this.saveIndex], this.data);
+    this.data = {};
+  }
+  closeEdit() {
+    this.data = {};
+  }
+
 }
